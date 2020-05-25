35988e1802871141413451ca636905e31c1e7c9f
Nathan Darraj <nadarrah15@gmail.com>
2020-05-25 18:29:01 -0400

    reworking to register other twitter users

--- bot.js
+++ bot.js
@@ -1,19 +1,29 @@
 var Discord = require('discord.io');
-var auth = require('./auth.json');
+var auth = require('../auth.json');
 var OAuth = require('oauth');
-var twitterSecrets = require('./oauthKeys.json');
+var twitterSecrets = require('../oauthKeys.json');
+var twitterDict = require('./twitterFeeds.json');
+var fs = require('fs');
 
 var timeToCheckNextTweet;
-var tweetID;
 var lastTweet = "";
 
-function checkIfUpdateTweet(firstTimeRun){
-	if (firstTimeRun || new Date(timeToCheckNextTweet.getTime() + 60000) <= new Date()) {
-		updateTweets();
+function checkIfUpdateTweet(screenName){
+	for (feed in twitterDict.feeds) {
+		if (feed.screen_name === screenName) {
+			var lastChecked = new Date(feed.last_checked);
+			//checks if updated within the last minute. To avoid rate limiting, pull once per minute
+			if(new Date(lastChecked.getTime() + 60000) <= new Date()) {
+				updateTweets(screenName);
+			}
+			else {
+				lastTweet = feed.last_tweet;
+			}
+		}
 	}
 }
 
-function updateTweets() {
+function updateTweets(screenName) {
 	
 	var oauth = new OAuth.OAuth(
 		'https://api.twitter.com/oauth/request_token',
@@ -26,12 +36,33 @@ function updateTweets() {
 	);
 	
 	oauth.get(
-		'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=DOTA2&count=1',
+		'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name='+ screenName + '&count=1',
 		twitterSecrets.token, //test user token
 		twitterSecrets.token_secret, //test user secret            
 		function (e, data, res){
+			if (e) console.error(e);
 			var response = JSON.parse(data);
-			lastTweet = 'https://twitter.com/DOTA2/status/' + response[0].id_str;
+			lastTweet = 'https://twitter.com/' + screenName + '/status/' + response[0].id_str;
+			
+			fs.readFile('./DiscordTwitterBot/twitterFeeds.json', 'utf8', function(err, data){
+				if (err) {
+					console.error(err);
+				} 
+				else {
+					obj = JSON.parse(data);
+					obj.feeds.push({
+						screen_name: screenName,
+						last_tweet: lastTweet,
+						last_checked: (new Date()).toString()
+					});
+					json = JSON.stringify(obj);
+					fs.writeFile('./DiscordTwitterBot/twitterFeeds.json', json, 'utf8', function(err, data){
+						if (err) console.error(err);
+					});
+				}
+			});
+			
+			twitterDict = require('./twitterFeeds');
 		}
 	);
 }
@@ -42,32 +73,116 @@ var bot = new Discord.Client({
 });
 bot.on('ready', function (evt) {
 	timeToCheckNextTweet = new Date();
-	updateTweets(true);
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
 	if (message.substring(0, 2) == '**') {
 		var args = message.substring(2).split(' ');
 		var cmd = args[0];
 		
-		args = args.splice(1);
+		args = args[1];
 		switch(cmd) {
 			case 'status': 
-				updateTweets(false);
+				//precondition: needs a twitter handle
+				if (args === '') {
+					bot.sendMessage({
+						to: channelID,
+						message: 'Please use a valid twitter handle'
+					});
+				}
+				else {
+					checkIfUpdateTweet(false, args);
+					bot.sendMessage({
+						to: channelID,
+						message: lastTweet
+					});
+				}
+			break;
+			case 'register':
+				//precondition: needs a twitter handle
+				if (args === ''){
+					bot.sendMessage({
+						to: channelID,
+						message: 'please enter a valid twitter handle'
+					});
+				}
+				else {
+					fs.readFile('./DiscordTwitterBot/twitterFeeds.json', 'utf8', function(err, data){
+						if (err) {
+							console.error(err);
+						} 
+						else {
+							obj = JSON.parse(data);
+							var feedExists = false;
+							for (feed in obj.feeds) {
+								if(feed.screen_name = args) {
+									feedExists = true;
+									break;
+								}
+							}
+							
+							if (feedExists) {
+								bot.sendMessage({
+									to: channelID,
+									message: 'Twitter handle is already registered'
+								});
+							}
+							else {
+								updateTweets(args);
+
+								bot.sendMessage({
+									to: channelID,
+									message: '`' + args + '` twitter handle registered'
+								});
+							}
+						}
+					});	
+				}
+			break;
+			case 'feeds':
+				fs.readFile('./DiscordTwitterBot/twitterFeeds.json', 'utf8', function(err, data){
+					if (err) {
+						console.error(err);
+					} 
+					else {
+						obj = JSON.parse(data);
+						var output = "";
+						for (feed in obj.feeds) {
+							output += '`' + feed.screen_name + '` ';
+						}
+						
+						bot.sendMessage({
+							to: channelID,
+							message: output
+						});
+					}
+				});
+				var data = JSON.parse(twitterDict);
+				var output = "";
+				for (feed in data.feeds) {
+					output += '`' + feed.screen_name + '` ';
+				}
+				
 				bot.sendMessage({
 					to: channelID,
-					message: lastTweet
+					message: output
 				});
-			break;
+			break;	
 			case 'tony':
 				bot.sendMessage({
 					to: channelID,
 					message: 'fuck tony'
 				});
 			break;
+			case 'help':
+				bot.sendMessage({
+					to: channelID,
+					message: 'Commands: `status [twitter handle]` `register [twitter handle]` `feeds` `tony`. When entering a twitter handle, do **NOT** use the `@` symbol'
+				});
+			break;
 			default:
 				bot.sendMessage({
 					to: channelID,
-					message: 'unknown command. Known commands: status'
+					message: 'unknown command. Known commands: `status [twitter handle]` `register [twitter handle]` `feeds` `tony`'
 				});
 			break;
 		}
